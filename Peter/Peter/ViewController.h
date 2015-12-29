aa9a99dec0ae5f66f4cf18e705a5dc3c156f15cd
han <han@qq.com>
2015-12-29 15:28:33 +0800

    提交

---
+++ Peter/Peter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Peter
+//
+//  Created by hanjuanqi on 15/12/29.
+//  Copyright © 2015年 hanjuanqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Peter/Peter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Peter
+//
+//  Created by hanjuanqi on 15/12/29.
+//  Copyright © 2015年 hanjuanqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
